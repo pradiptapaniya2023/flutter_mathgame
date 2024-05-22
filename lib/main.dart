e95742f6c5319a55363cd5eac09ec8e6dc80c20c
Pradip Tapaniya <pradiptapaniya2023.dev@gmail.com>
2024-05-22 17:27:45 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -6,28 +6,3 @@ void main() {
     home: First_Page(),
   ));
 }
-
-
-// error nu hu have mari rite joi laish github mathi project lai ne
-// e project ly j leje atyre km ke hu hmna psi e dlt kari des km ke aama error che ne atle , error slove thay ne psi p[aso new upload karis
-
-// error solve kari ne lkhali push karva no hoi code
-// jo atyre github ma aa comment nay hoi apde vat kari e
-
-// aa project github mathi delete ny karto jo marathi error solve thay jase to hu mari side thi push kari daish
-
-// atle aa bey ma dekhay atle bey access kari skye avu kari ankhu che te ?
-// ha pan tu je change kare e mare jova hoi mara android studio ma to pela project ne pull karvo pade to aave
-// pull atle
-// github.com pr je live hoi e apda android studio ma update krva mate git pull ave
-// git clone e j ke ?
-// git clone thi akho project new duplicate copy bane
-// nd pull thi already je project hoi ama change khali thay am ne
-// jm ke apda bey ma project che mai change karo tare e downalon d no karvo hoi aakho atle tu khali pull kare atle je change hhoi e aavi jaay ? am
-// ha ani mate j git pull che k vare vare akho project no banav vo pade
-// kn okay , tu anu access tari pase j rakhis ke psi e change kari nakhu error slove thay psi ?
-// aa project math_pzl nu j mari pase rese baki tu je new upload kare anu ny hoi atle anu revade je
-// okay 👍
-
-
-// have jo hu github ma change akru chu but e tara android studio ma ny ave jya sudhi tu pull ny kare tya sudhi
