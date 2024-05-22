fe454973c4819b0060f775da932b90f4a6568db7
Pradip Tapaniya <pradiptapaniya2023.dev@gmail.com>
2024-05-22 17:14:58 +0530

    12

--- lib/main.dart
+++ lib/main.dart
@@ -6,3 +6,10 @@ void main() {
     home: First_Page(),
   ));
 }
+
+
+// error nu hu have mari rite joi laish github mathi project lai ne
+// e project ly j leje atyre km ke hu hmna psi e dlt kari des km ke aama error che ne atle , error slove thay ne psi p[aso new upload karis
+
+// error solve kari ne lkhali push karva no hoi code
+// jo atyre github ma aa comment nay hoi apde vat kari e
