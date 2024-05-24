0ea2c8f8feb3d62477c9abc0e5f7b51d5b32535c
Pradip Tapaniya <pradiptapaniya2023.dev@gmail.com>
2024-05-24 13:16:16 +0530

    Manage pageview inside gridview (range)

--- lib/fourthpage.dart
+++ lib/fourthpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mathgame/secondpage.dart';
 
@@ -10,25 +11,8 @@ class Fourth_Page extends StatefulWidget {
 }
 
 class State_Fourth_Page extends State<Fourth_Page> {
-  int levelPerPage = 28;
-  int totalLevel = 75;
-
-  List pageViewFun(int levelPerPage, int totalLevel) {
-    List startingLevelList = List.generate(totalLevel, (index) => index + 1);
-    List showLevelNumber = [];
-
-    for (int i = 0; i < totalLevel; i = i + levelPerPage) {
-      int end = (i + levelPerPage < totalLevel) ? i + levelPerPage : totalLevel;
-      showLevelNumber.add(startingLevelList.sublist(i, end));
-    }
-
-    return showLevelNumber;
-  }
-
   @override
   Widget build(BuildContext context) {
-    List pageViewList = pageViewFun(levelPerPage, totalLevel);
-
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -56,58 +40,152 @@ class State_Fourth_Page extends State<Fourth_Page> {
             ),
             Expanded(
               child: PageView.builder(
-                itemCount: pageViewList.length,
+                itemCount: 3,
                 itemBuilder: (context, pageIndex) {
-                  print(
-                      '=== Page $pageIndex: ${pageViewList[pageIndex].length} items');
+                  int gridViewItemCount = 0;
+                  int startIndex = 0;
+
+                  if (pageIndex == 0) {
+                    gridViewItemCount = 28;
+                    startIndex = 1;
+                  } else if (pageIndex == 1) {
+                    gridViewItemCount = 28;
+                    startIndex = 29;
+                  } else {
+                    gridViewItemCount = 19;
+                    startIndex = 57;
+                  }
+
                   return GridView.builder(
                     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                         crossAxisCount: 4),
-                    itemCount: pageViewList[pageIndex].length,
+                    shrinkWrap: true,
+                    itemCount: gridViewItemCount,
                     itemBuilder: (context, gridIndex) {
-                      int level = pageViewList[pageIndex][gridIndex];
                       int currentLevel = Second_Page.levelNum;
-                      String state = Second_Page.levelStateList[level - 1];
+                      int pageStartingIndex = startIndex + gridIndex;
+                      String state =
+                          Second_Page.levelStateList[pageStartingIndex];
 
-                      return (Container(
-                        margin: EdgeInsets.all(5),
-                        height: 30,
-                        width: 30,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          border: Border.all(color: Colors.black, width: 2),
-                        ),
-                        child: Center(
-                          child: InkWell(
-                            onDoubleTap: () {},
-                            child: Container(
-                              height: 65,
-                              width: 65,
-                              decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      image: AssetImage(state == "clear"
-                                          ? "asset/images/tick.png"
-                                          : (state == "skip"
-                                              ? " "
-                                              : "asset/images/lock.png")))),
-                              child: Center(
-                                child: currentLevel == level ||
-                                        state == "clear" ||
-                                        state == "skip"
-                                    ? Text(
-                                        ' ${pageViewList[pageIndex][gridIndex]}',
-                                        style: TextStyle(
-                                            color: Colors.grey[700],
-                                            fontFamily: 'mathGameFont',
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 40),
-                                      )
-                                    : null,
+                      if (pageIndex == 0) {
+                        return Container(
+                          margin: EdgeInsets.all(5),
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(color: Colors.black, width: 2),
+                          ),
+                          child: Center(
+                            child: InkWell(
+                              onDoubleTap: () {},
+                              child: Container(
+                                height: 65,
+                                width: 65,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage(state == "clear"
+                                            ? "asset/images/tick.png"
+                                            : (state == "skip" ||
+                                                    currentLevel ==
+                                                        gridIndex + 1
+                                                ? ""
+                                                : "asset/images/lock.png")))),
+                                child: Center(
+                                  child: currentLevel == gridIndex + 1 ||
+                                          state == "clear" ||
+                                          state == "skip"
+                                      ? Text(
+                                          ' ${gridIndex + 1}',
+                                          style: TextStyle(
+                                              color: Colors.grey[700],
+                                              fontFamily: 'mathGameFont',
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 40),
+                                        )
+                                      : null,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ));
+                        );
+                      } else if (pageIndex == 1) {
+                        return Container(
+                          margin: EdgeInsets.all(5),
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(color: Colors.black, width: 2),
+                          ),
+                          child: Center(
+                            child: InkWell(
+                              onDoubleTap: () {},
+                              child: Container(
+                                height: 65,
+                                width: 65,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage(state == "clear"
+                                            ? "asset/images/tick.png"
+                                            : (state == "skip" ||
+                                                    currentLevel ==
+                                                        gridIndex + 1
+                                                ? ""
+                                                : "asset/images/lock.png")))),
+                                child: Center(
+                                  child: Text(
+                                    ' ${gridIndex + 29}',
+                                    style: TextStyle(
+                                        color: Colors.grey[700],
+                                        fontFamily: 'mathGameFont',
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 40),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      } else {
+                        return Container(
+                          margin: EdgeInsets.all(5),
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(color: Colors.black, width: 2),
+                          ),
+                          child: Center(
+                            child: InkWell(
+                              onDoubleTap: () {},
+                              child: Container(
+                                height: 65,
+                                width: 65,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage(state == "clear"
+                                            ? "asset/images/tick.png"
+                                            : (state == "skip" ||
+                                                    currentLevel ==
+                                                        gridIndex + 1
+                                                ? ""
+                                                : "asset/images/lock.png")))),
+                                child: Center(
+                                  child: Text(
+                                    ' ${gridIndex + 57}',
+                                    style: TextStyle(
+                                        color: Colors.grey[700],
+                                        fontFamily: 'mathGameFont',
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 40),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      }
                     },
                   );
                 },
